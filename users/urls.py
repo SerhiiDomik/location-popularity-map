f2167465d7cd20878e77f010e68d3133b879ad98
Serhii <serhii.domik@gmail.com>
2025-08-07 16:36:21 +0300

    Implemented user password reset via email

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from users.views import RegisterView, LoginView, LogoutView
+from users.views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+)
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
